679dd9d7e19ab562a891ed4a8d99f7b19145a6e0
“joshuab3” <joshua.george@b3.se>
2022-10-05 11:50:22 +0200

    Added job page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector("nav");
 const logo = document.getElementById("grit-logo");
+let dropArea = document.getElementById('drop-area')
+
 
 //The function is invoked on the hashchange event,
 //which is fired when the fragment identifier of the URL has changed (the part of the URL beginning with and following the # symbol)
@@ -25,4 +27,11 @@ function changeColorOnLoad() {
   } else if (location.hash == "#contact") {
     nav.classList.add("changeColor");
   }
+}
+
+function getFileName(fileName){
+
+  var newFileName=fileName.replace(/^.*\\/,"");
+  $('#display-file-name').html(newFileName)
+
 }
